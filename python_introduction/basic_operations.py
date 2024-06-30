7776c067ca313352d5064936fceddc98a0096e70
diana nthenya odhiambo <dianaodhiambo1096@gmail.com>
2024-06-30 21:20:54 +0300

    Add basic arithmetic operations script

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# Define the variables
+number1 = 10
+number2 = 5
+
+# Perform the operations
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+# print the results
+print(addition)
+print(subtraction)
+print(multiplication)
